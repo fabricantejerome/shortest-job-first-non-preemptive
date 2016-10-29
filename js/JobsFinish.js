abdb8709b75001f742022686826f3b8a29d04f69
Jerome Fabricante <fabricantejerome@gmail.com>
2016-10-29 22:26:34 +0800

    Override the parent function displayJobs

--- js/JobsFinish.js
+++ js/JobsFinish.js
@@ -2,4 +2,22 @@
 
 class JobsFinish extends Jobs {
 
+	// Display the Jobs attribute
+	displayJobs() {
+		let table_content = '';
+
+		for (let [index, value] of this.data.entries()) {
+			table_content = table_content + `<tr data-index=${index} >
+							<td> ${value.job} </td>
+							<td> ${value.arrival} </td> 
+							<td> ${value.burst}</td>
+							<td> ${value.waiting}</td>
+							<td> ${value.finish}</td>
+							</tr>`;
+		}
+
+		return table_content;
+		
+	}
+
 }
